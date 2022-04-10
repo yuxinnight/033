6939932dde02b16f66e4ae5057b221b42686c1e6
yuxinnight <103322172+yuxinnight@users.noreply.github.com>
2022-04-10 23:57:35 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -344,7 +344,7 @@ function shareText(score) {
     if (score <= 49) return '你真的太遜了';
     if (score <= 99) return '唉呦點的不錯嘛';
     if (score <= 149) return '真會點';
-    if (score <= 199) return '張乃介';
+    if (score <= 199) return '超會點李育有';
     return 'やりますね';
 }
 
